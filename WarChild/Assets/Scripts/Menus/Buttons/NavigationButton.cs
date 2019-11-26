a2994b49c373e122eb18062cfc8b533e54bde8c1
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-11-26 18:18:53 -0500

    navigation menu now loads levels

--- WarChild/Assets/Scripts/Menus/Buttons/NavigationButton.cs
+++ WarChild/Assets/Scripts/Menus/Buttons/NavigationButton.cs
@@ -1,36 +1,8 @@
 ﻿
 public class NavigationButton : UIButton
 {
-    private void Awake()
-    {
-        UIManager.OpenNavigation += OpenNavigation;
-        UIManager.CloseNavigation += CloseNavigation;
-        UIManager.OpenArsenal += OpenArsenal;
-        UIManager.CloseArsenal += CloseArsenal;
-    }
-
     public override void Activate()
     {
         UIManager.OpenNav();
-    }
-
-    private void OpenNavigation()
-    {
-        gameObject.SetActive(false);
-    }
-
-    private void CloseNavigation()
-    {
-        gameObject.SetActive(true);
-    }
-
-    private void OpenArsenal()
-    {
-        gameObject.SetActive(false);
-    }
-
-    private void CloseArsenal()
-    {
-        gameObject.SetActive(true);
     }
 }
